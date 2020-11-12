fa0d8c6c390fe42a39dbb999c680d10283f9566e
Daniel Graf <daniel@dedicatedcode.com>
2020-11-12 08:46:46 +0100

    #16: fix

--- deploy-to-docker-hub.sh
+++ deploy-to-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd api && ./gradlew build && sudo docker build . -t dedicatedcode/paperspace:latest && sudo docker push dedicatedcode/paperspace:latest
+cd api && ./gradlew clean build && sudo docker build . -t dedicatedcode/paperspace:latest && sudo docker push dedicatedcode/paperspace:latest
 cd ..
 cd search && sudo docker build . -t dedicatedcode/paperspace-search:latest && sudo docker push dedicatedcode/paperspace-search:latest
 cd ..
